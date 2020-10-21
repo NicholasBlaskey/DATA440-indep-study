b5448ddca1006314f15c0c89a763333ce7020680
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2020-10-21 13:37:00 -0400

    Replaced texture2D -> texture in shaders

--- fluid/main.go
+++ fluid/main.go
@@ -320,10 +320,10 @@ const curlShader = `
     uniform sampler2D uVelocity;
 
     void main () {
-        float L = texture2D(uVelocity, vL).y;
-        float R = texture2D(uVelocity, vR).y;
-        float T = texture2D(uVelocity, vT).x;
-        float B = texture2D(uVelocity, vB).x;
+        float L = texture(uVelocity, vL).y;
+        float R = texture(uVelocity, vR).y;
+        float T = texture(uVelocity, vT).x;
+        float B = texture(uVelocity, vB).x;
         float vorticity = R - L - T + B;
         FragColor = vec4(0.5 * vorticity, 0.0, 0.0, 1.0);
     }
@@ -349,18 +349,18 @@ const vorticityShader = `
     uniform float dt;
 
     void main () {
-        float L = texture2D(uCurl, vL).x;
-        float R = texture2D(uCurl, vR).x;
-        float T = texture2D(uCurl, vT).x;
-        float B = texture2D(uCurl, vB).x;
-        float C = texture2D(uCurl, vUv).x;
+        float L = texture(uCurl, vL).x;
+        float R = texture(uCurl, vR).x;
+        float T = texture(uCurl, vT).x;
+        float B = texture(uCurl, vB).x;
+        float C = texture(uCurl, vUv).x;
 
         vec2 force = 0.5 * vec2(abs(T) - abs(B), abs(R) - abs(L));
         force /= length(force) + 0.0001;
         force *= curl * C;
         force.y *= -1.0;
 
-        vec2 velocity = texture2D(uVelocity, vUv).xy;
+        vec2 velocity = texture(uVelocity, vUv).xy;
         velocity += force * dt;
         velocity = min(max(velocity, -1000.0), 1000.0);
         FragColor = vec4(velocity, 0.0, 1.0);
@@ -383,12 +383,12 @@ const divergenceShader = `
     uniform sampler2D uVelocity;
 
     void main () {
-        float L = texture2D(uVelocity, vL).x;
-        float R = texture2D(uVelocity, vR).x;
-        float T = texture2D(uVelocity, vT).y;
-        float B = texture2D(uVelocity, vB).y;
+        float L = texture(uVelocity, vL).x;
+        float R = texture(uVelocity, vR).x;
+        float T = texture(uVelocity, vT).y;
+        float B = texture(uVelocity, vB).y;
 
-        vec2 C = texture2D(uVelocity, vUv).xy;
+        vec2 C = texture(uVelocity, vUv).xy;
         if (vL.x < 0.0) { L = -C.x; }
         if (vR.x > 1.0) { R = -C.x; }
         if (vT.y > 1.0) { T = -C.y; }
@@ -412,7 +412,7 @@ const clearShader = `
     uniform float value;
 
     void main () {
-        FragColor = value * texture2D(uTexture, vUv);
+        FragColor = value * texture(uTexture, vUv);
     }
 `
 const copyShader = `
@@ -427,7 +427,7 @@ const copyShader = `
     uniform sampler2D uTexture;
 
     void main () {
-        FragColor = texture2D(uTexture, vUv);
+        FragColor = texture(uTexture, vUv);
     }
 `
 
@@ -448,12 +448,12 @@ const pressureShader = `
     uniform sampler2D uDivergence;
 
     void main () {
-        float L = texture2D(uPressure, vL).x;
-        float R = texture2D(uPressure, vR).x;
-        float T = texture2D(uPressure, vT).x;
-        float B = texture2D(uPressure, vB).x;
-        float C = texture2D(uPressure, vUv).x;
-        float divergence = texture2D(uDivergence, vUv).x;
+        float L = texture(uPressure, vL).x;
+        float R = texture(uPressure, vR).x;
+        float T = texture(uPressure, vT).x;
+        float B = texture(uPressure, vB).x;
+        float C = texture(uPressure, vUv).x;
+        float divergence = texture(uDivergence, vUv).x;
         float pressure = (L + R + B + T - divergence) * 0.25;
         FragColor = vec4(pressure, 0.0, 0.0, 1.0);
     }
@@ -476,11 +476,11 @@ const gradientSubtractShader = `
     uniform sampler2D uVelocity;
 
     void main () {
-        float L = texture2D(uPressure, vL).x;
-        float R = texture2D(uPressure, vR).x;
-        float T = texture2D(uPressure, vT).x;
-        float B = texture2D(uPressure, vB).x;
-        vec2 velocity = texture2D(uVelocity, vUv).xy;
+        float L = texture(uPressure, vL).x;
+        float R = texture(uPressure, vR).x;
+        float T = texture(uPressure, vT).x;
+        float B = texture(uPressure, vB).x;
+        vec2 velocity = texture(uVelocity, vUv).xy;
         velocity.xy -= vec2(R - L, T - B);
         FragColor = vec4(velocity, 0.0, 1.0);
     }
@@ -508,10 +508,10 @@ const advectionShader = `
         vec2 iuv = floor(st);
         vec2 fuv = fract(st);
 
-        vec4 a = texture2D(sam, (iuv + vec2(0.5, 0.5)) * tsize);
-        vec4 b = texture2D(sam, (iuv + vec2(1.5, 0.5)) * tsize);
-        vec4 c = texture2D(sam, (iuv + vec2(0.5, 1.5)) * tsize);
-        vec4 d = texture2D(sam, (iuv + vec2(1.5, 1.5)) * tsize);
+        vec4 a = texture(sam, (iuv + vec2(0.5, 0.5)) * tsize);
+        vec4 b = texture(sam, (iuv + vec2(1.5, 0.5)) * tsize);
+        vec4 c = texture(sam, (iuv + vec2(0.5, 1.5)) * tsize);
+        vec4 d = texture(sam, (iuv + vec2(1.5, 1.5)) * tsize);
 
         return mix(mix(a, b, fuv.x), mix(c, d, fuv.x), fuv.y);
     }
@@ -521,8 +521,8 @@ const advectionShader = `
         vec2 coord = vUv - dt * bilerp(uVelocity, vUv, texelSize).xy * texelSize;
         vec4 result = bilerp(uSource, coord, dyeTexelSize);
     #else
-        vec2 coord = vUv - dt * texture2D(uVelocity, vUv).xy * texelSize;
-        vec4 result = texture2D(uSource, coord);
+        vec2 coord = vUv - dt * texture(uVelocity, vUv).xy * texelSize;
+        vec4 result = texture(uSource, coord);
     #endif
         float decay = 1.0 + dissipation * dt;
         FragColor = result / decay;
@@ -569,13 +569,13 @@ const displayShader = `
     }
 
     void main () {
-        vec3 c = texture2D(uTexture, vUv).rgb;
+        vec3 c = texture(uTexture, vUv).rgb;
 
     #ifdef SHADING
-        vec3 lc = texture2D(uTexture, vL).rgb;
-        vec3 rc = texture2D(uTexture, vR).rgb;
-        vec3 tc = texture2D(uTexture, vT).rgb;
-        vec3 bc = texture2D(uTexture, vB).rgb;
+        vec3 lc = texture(uTexture, vL).rgb;
+        vec3 rc = texture(uTexture, vR).rgb;
+        vec3 tc = texture(uTexture, vT).rgb;
+        vec3 bc = texture(uTexture, vB).rgb;
 
         float dx = length(rc) - length(lc);
         float dy = length(tc) - length(bc);
@@ -588,11 +588,11 @@ const displayShader = `
     #endif
 
     #ifdef BLOOM
-        vec3 bloom = texture2D(uBloom, vUv).rgb;
+        vec3 bloom = texture(uBloom, vUv).rgb;
     #endif
 
     #ifdef SUNRAYS
-        float sunrays = texture2D(uSunrays, vUv).r;
+        float sunrays = texture(uSunrays, vUv).r;
         c *= sunrays;
     #ifdef BLOOM
         bloom *= sunrays;
@@ -600,7 +600,7 @@ const displayShader = `
     #endif
 
     #ifdef BLOOM
-        float noise = texture2D(uDithering, vUv * ditherScale).r;
+        float noise = texture(uDithering, vUv * ditherScale).r;
         noise = noise * 2.0 - 1.0;
         bloom += noise / 255.0;
         bloom = linearToGamma(bloom);
@@ -632,7 +632,7 @@ const splatShader = `
         vec2 p = vUv - point.xy;
         p.x *= aspectRatio;
         vec3 splat = exp(-dot(p, p) / radius) * color;
-        vec3 base = texture2D(uTarget, vUv).xyz;
+        vec3 base = texture(uTarget, vUv).xyz;
         FragColor = vec4(base + splat, 1.0);
     }
 `
